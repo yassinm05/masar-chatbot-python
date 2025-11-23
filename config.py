b38458aaa7a76927f37017b79caf837a90bbf08b
yassinm05 <yassinmahmoudmostafa@gmail.com>
2025-11-23 08:27:05 +0200

    deleting the hardcoded key

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
     # Try to get from Environment Variables (Production), or fallback to hardcoded (Development)
-    GEMINI_API_KEY = os.environ.get("Masar-Skills", "AIzaSyDqxQbXQ86PZh17gi2cAMAfc2Q9JSY06KU")
+    GEMINI_API_KEY = os.environ.get("Masar-Skills")
     
     # The URL of your deployed .NET API
     # If testing locally, use http://localhost:5236 or your ngrok URL
